7d0c7df631b8991be38ba972187dcca789bb7ae1
hyywestwood <3140105713@zju.edu.cn>
2020-09-02 19:15:16 +0800

    尝试构造ajax来获取水质数据

---
+++ ashx.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/9/2 15:43
+# @Author  : hyy
+# @Email   : hyywestwood@zju.edu.cn
+# @File    : ashx.py
+# @Software: PyCharm
+"""
+本程序尝试分析水质数据实时发布系统的网络请求，
+采用直接构造请求的方式获取水质数据
+从而省去了selenium的使用
+"""
+import os
+import requests
+import time
+import json
+import re
+
+headers = {
+    'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+    'Connection': 'Keep-Alive',
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'Host': '106.37.208.243:8068',
+    'Origin': 'http://106.37.208.243:8068',
+    'Referer': 'http://106.37.208.243:8068/GJZ/Business/Publish/RealData.html',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:80.0) Gecko/20100101 Firefox/80.0',
+}
+data = {
+    'AreaID': '',
+    'RiverID': '',
+    'MNName': '',
+    'PageIndex': '1',
+    'PageSize': '60',
+    'action': 'getNewData'
+}
+time.sleep(5)
+url = 'http://106.37.208.243:8068/GJZ/Ajax/Publish.ashx'
+# print(url)
+r = requests.post(url,data=data, headers=headers)
+data_get = json.loads(r.text)
+
+pattern = re.compile(r'<[^>]+>',re.S)
+variables_name = list(map(lambda x: pattern.sub('', x), data_get["thead"]))
+
+result = []
+for item in data_get["tbody"]:
+    item = list(map(lambda x: ('' if x is None else x), item))
+    result.append(list(map(lambda x: pattern.sub('', x), item)))
+
+# 下面开始写文件，代码比使用selenium少很多
+assert result is not None
+path2 = os.path.join('.', '水质数据', '新版')
+folder = os.path.exists(path2)
+if not folder:
+    os.makedirs(path2)
+
+for hang in result:
+    path3 = os.path.join(path2, hang[0])
+    folder = os.path.exists(path3)
+    if not folder:
+        os.makedirs(path3)
+    with open(path3 + '\\{}-{}.txt'.format(hang[1], hang[2]), 'a+', encoding='utf-8') as f:
+        f.write('{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t \n'.format(
+            time.strftime("%Y-", time.localtime()) + hang[3],
+            hang[4], hang[5], hang[6], hang[7], hang[8], hang[9], hang[10], hang[11], hang[12], hang[13],
+            hang[14], hang[15], hang[16]))
